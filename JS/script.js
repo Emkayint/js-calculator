038d2620fa5ce39192668ec6bfdc815023a9e100
Martin <mkmartinoes@gmail.com>
2022-03-10 11:13:40 +0300

    ft: display text at a time

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+(function($){
+    $.fn.writeText = function(content){
+        let contentArr = content.split(""),
+            current = 0,
+            elem = tthis;
+        setInterval(function(){
+            if(current < contentArr.length){
+                elem.text(elem.text() + contentArr[current++])
+            }
+        }, 100);
+    }
+})(jQuery);
+
+$('p').writeText("This is My Name")
