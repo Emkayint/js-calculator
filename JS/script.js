34d6bd94eae1df39c34c409703f8e4707e139ee6
Martin <mkmartinoes@gmail.com>
2022-03-10 14:15:51 +0300

    ft: sexon Line animation

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,29 @@
 setInterval(function showChar(){
     let elem = document.getElementById('demo');
-    let i = 0;
+    let i = -1;
     let myText = "Work with no play makes Jack a dull boy";
+    let x = -1;
+    let elem1 = document.getElementById('pro');
 
     (function start(){
         if(i < myText.length){
             demo.innerHTML = demo.innerHTML + myText.charAt(i++);
-            setTimeout(start, 50);
+            setTimeout(start, 150);
         }
     })();
     demo.innerHTML = "";
-})
+
+    setInterval( function showChar2(){
+        (function programmer(){
+            let str = "I am a programmer";
+    
+            if(x < str.length){
+                pro.innerHTML = pro.innerHTML + str.charAt(x++);
+                setTimeout(programmer, 100);
+            }
+        })();
+        pro.innerHTML = "";
+    }, 4000);
+}, 8000)
 
 
