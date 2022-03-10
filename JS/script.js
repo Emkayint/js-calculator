8d0f7b9ae6f54d08a67724e14e28f7fe49b2181a
Martin <mkmartinoes@gmail.com>
2022-03-10 13:46:48 +0300

    ft: display text

--- JS/script.js
+++ JS/script.js
@@ -2,13 +2,15 @@
     $.fn.writeText = function(content){
         let contentArr = content.split(""),
             current = 0,
-            elem = tthis;
+            elem = this;
         setInterval(function(){
             if(current < contentArr.length){
                 elem.text(elem.text() + contentArr[current++])
             }
         }, 100);
-    }
+    };
 })(jQuery);
 
-$('p').writeText("This is My Name")
+$("#p").writeText("This is My Name");
+
+
