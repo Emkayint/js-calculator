afbf92c621dab6dc66f2ef1a4371fa5b433e3d29
Martin <mkmartinoes@gmail.com>
2022-03-10 14:41:46 +0300

    fix: display both lines

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,6 @@ setInterval(function showChar(){
     let elem = document.getElementById('demo');
     let i = -1;
     let myText = "Work with no play makes Jack a dull boy";
-    let x = -1;
-    let elem1 = document.getElementById('pro');
 
     (function start(){
         if(i < myText.length){
@@ -13,17 +11,21 @@ setInterval(function showChar(){
     })();
     demo.innerHTML = "";
 
-    setInterval( function showChar2(){
-        (function programmer(){
-            let str = "I am a programmer";
-    
-            if(x < str.length){
-                pro.innerHTML = pro.innerHTML + str.charAt(x++);
-                setTimeout(programmer, 100);
-            }
-        })();
-        pro.innerHTML = "";
-    }, 4000);
-}, 8000)
+}, 8000);
 
+(function showChar2(){
+    let x = -1;
+    let str = "I am a programmer";
+    let elem1 = document.getElementById('pro');
+
+    (function programmer(){
+        if(x < str.length){
+            pro.innerHTML = pro.innerHTML + str.charAt(x++);
+            setTimeout(programmer, 100);
+        }
+    })();
+    pro.innerHTML = "";
+})();
+
+// showChar2();
 
